e6ef10376ca5cd2e9cf1853c00158f665330a00d
Karan Desai <karandesai_96@live.com>
2016-06-02 14:11:12 +0530

    Add setup.py and label v0.1.0

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='yolog',
+    version='0.1.0',
+    description='Beautify your git logs!',
+    url='http://github.com/karandesai-96/yolog',
+    author='Karan Desai',
+    author_email='karandesai281196@gmail.com',
+    license='MIT',
+    packages=['yolog'],
+    zip_safe=True
+)

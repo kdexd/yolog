2b24ec1dd719f2a971fdda31898f167ceed76ffe
Karan Desai <karandesai_96@live.com>
2016-07-14 12:48:54 +0530

    Print help description through yolog generator.

--- yolog/yolog_generator.py
+++ yolog/yolog_generator.py
@@ -5,20 +5,54 @@ except ImportError:
     from configparser import SafeConfigParser
 
 
-
-RESET  = "$(tput sgr0)"
+RESET = "$(tput sgr0)"
 BACKSPACE = "%x08"
+
+help_description = ("""
+{yellow}Yolog - Beautify your Git logs !
+{white}--------------------------------
+{cyan}Usage: yolog [<additional optional commands>]
+
+{green}1. yolog -n
+{white}-      Display recent n commits.
+
+{green}2. yolog --skip=n
+{white}-      Skip recent n commits and display further.
+
+{green}3. yolog --author=john
+{white}-      Filter commits according to author.
+{white}       Part of name / whole will be accepted.
+
+{green}4. yolog --before=dd-mmm-yyyy
+{green}   yolog --until=dd/mmm/yyyy
+{white}-      Display commits before this date.
+
+{green}5. yolog --after=dd/mmm/yyyy
+{green}   yolog --since=dd-mmm-yyyy
+{white}-      Display commits after this date.
+
+{green}6. yolog --grep="foo\ bar"
+{white}-      Display commits with "foo bar" in their description.
+
+{green}7. yolog --help
+{green}   yolog -h
+{white}-      Display this instruction.
+
+{cyan}* Any of these can be combined together and used.
+{cyan}* Regular expressions are also accepted in grep and author.
+{cyan}* Use escape character if using whitespace: yolog --grep="fixes\ bug"
+{reset}""")
 
 
 class YologGenerator(object):
-    BLACK  = "$(tput bold)$(tput setaf 0)"
-    RED    = "$(tput bold)$(tput setaf 1)"
-    GREEN  = "$(tput bold)$(tput setaf 2)"
+    BLACK = "$(tput bold)$(tput setaf 0)"
+    RED = "$(tput bold)$(tput setaf 1)"
+    GREEN = "$(tput bold)$(tput setaf 2)"
     YELLOW = "$(tput bold)$(tput setaf 3)"
-    BLUE   = "$(tput bold)$(tput setaf 4)"
+    BLUE = "$(tput bold)$(tput setaf 4)"
     PURPLE = "$(tput bold)$(tput setaf 5)"
-    CYAN   = "$(tput bold)$(tput setaf 6)"
-    WHITE  = "$(tput setaf 7)"
+    CYAN = "$(tput bold)$(tput setaf 6)"
+    WHITE = "$(tput setaf 7)"
 
     def __init__(self, path):
         self.config = SafeConfigParser()
@@ -50,4 +84,12 @@ class YologGenerator(object):
             "column -t -s \";;\" | less -FXRS".format(
                 self._format, git_arguments
             )
-        ) 
+        )
+
+    def print_help(self):
+        return (
+            "echo \"{0}\" | less -RS".format(help_description.format(
+                yellow=self.YELLOW, green=self.GREEN, cyan=self.CYAN,
+                white=self.WHITE, reset=RESET
+            ))
+        )
